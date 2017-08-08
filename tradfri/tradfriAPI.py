0336e673bf0053027673851860c0343e53baa2eb
Andrea Prunic <aprunic@gmail.com>
2017-08-08 16:02:28 +0200

    - add module with API endpoints etc for readability

---
+++ tradfri/tradfriAPI.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# created by Floyd
+# helper module with API endpoints etc to ease writing the rest of code
+
+# define API endpoints 
+
+_DEVICE_DATA_ = "3311"
+_ONOFF_ = "5850"
+_ID_ = "9003"
+_NAME_ = "9001"
+_DIMMER_ = "5851"
+
+_STATE_ = ["Off", "On"]
+
+
