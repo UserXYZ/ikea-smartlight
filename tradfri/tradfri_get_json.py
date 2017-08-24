64695c2efc5100ac7df5e36d0a7b476a6ec94afc
Andrea Prunic <aprunic@gmail.com>
2017-08-24 14:02:03 +0200

    - return list of lists with controllable device data (lightbulbs) - return list of json representation of controllable device data (lightbulbs) - return list with json representation of all groups

---
+++ tradfri/tradfri_get_json.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+from tradfri import tradfriStatus
+from tradfri import tradfriAPI as API
+
+gldevs = []
+
+def get_ldevs(hubip, securityid):
+    lightdev = []
+    ldevs = []
+    devices = tradfriStatus.tradfri_get_devices(hubip, securityid)
+
+    for deviceid in (range(len(devices))):
+        lightdev.append(tradfriStatus.tradfri_get_lightdev(hubip, securityid, str(devices[deviceid])))
+
+    for _ in range(len(lightdev)):
+        try:
+	    devs = []
+	    st = lightdev[_][API._DEVICE_DATA_][0][API._ONOFF_]
+	    devtype = lightdev[_]["3"]["1"]
+	    brightness = int(round(lightdev[_][API._DEVICE_DATA_][0][API._DIMMER_]/2.54))
+	    id = lightdev[_][API._ID_]
+	    devname = lightdev[_][API._NAME_]
+	    state = API._STATE_[st]
+	    gldevs.append(id)
+	    devs = [id, devtype, devname, state, brightness]
+	    ldevs.append(devs)
+        except KeyError:
+            # device is not a lightbulb but a remote control, dimmer or sensor
+            pass
+    return ldevs
+
+def get_ldevs_json(hubip, securityid):
+    ldevs_json = []
+    ldevs = get_ldevs(hubip, securityid)
+
+    for _ in range(len(ldevs)):
+	ldevs_json.append(json.dumps({"ID":ldevs[_][0],
+    	    "DeviceType":ldevs[_][1],"DeviceName":ldevs[_][2],
+    	    "State":ldevs[_][3],
+    	    "Brightness":ldevs[_][4]}))
+    return ldevs_json
+
+def get_groups_json(hubip, securityid):
+    groups_json = []
+    lightgroup = []
+    groups = tradfriStatus.tradfri_get_groups(hubip, securityid)
+    ldevs = get_ldevs(hubip, securityid)
+
+    for groupid in (range(len(groups))):
+        lightgroup.append(tradfriStatus.tradfri_get_group(hubip, securityid, str(groups[groupid])))
+
+    for _ in range(len(lightgroup)):
+	    ggdev = []
+	    gdevs = lightgroup[_][API._HS_ACCESSORY_LINK_][API._HS_LINK_][API._ID_]
+	    gstate = lightgroup[_][API._ONOFF_]
+	    gbrightness = int(round(lightgroup[_][API._DIMMER_]/2.54))
+
+	    for g in gdevs:
+		for l in range(len(ldevs)):
+			if(g == ldevs[l][0]):
+			    ggdev.append(g)
+	    
+	    groups_json.append(json.dumps({"ID":lightgroup[_][API._ID_],
+		"GroupName":lightgroup[_][API._NAME_],
+		"State":API._STATE_[gstate],
+		"Brightness":gbrightness,
+		"Devices":ggdev}))
+	    
+    return groups_json
