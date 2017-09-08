96434cedfd2a926026dc21449e5ff7606621ccac
Andrea Prunic <aprunic@gmail.com>
2017-09-09 01:36:05 +0200

    - add try/except statements where needed - add on_unsubscrie() method - start work on decoding and acting upon messages - add comments

--- tradfri-mqtt.py
+++ tradfri-mqtt.py
@@ -10,92 +10,102 @@ cfg = os.getcwd()+'/tradfri.cfg'
 running = False
 
 class MyDaemon(Daemon):
+    # loop flag: True, loop running; False, loop stopped
     flag = True
-
+    # signal handler for TERM and INT signals from main process
     def sigh(self, signum, frame):
        if signum in (signal.SIGTERM, signal.SIGINT):
                 self.flag = False
-
+    # runner that we override
     def run(self):
 	global running
 	global client
-
+	# catch signals and call signal handler metdhod when caught
 	signal.signal(signal.SIGTERM, self.sigh)
 	signal.signal(signal.SIGINT, self.sigh)
-
+	# setup everything and connect, subscribe etc
 	mqtt_srv, mqtt_port, topic, hubip, securityid = setup()
 	client.connect(mqtt_srv, mqtt_port, 60)
-	client.subscribe(topic)
+	(result, mid) = client.subscribe([(topic+"/devices/todo", 1), (topic+"/groups/todo", 1)])
+	if result == mqtt.MQTT_ERR_SUCCESS:
+	    pass
+	else:
+    	    sys.stderr.write(result+'\n')
 	client.loop_start()
-	
+	time.sleep(0.2)
+	# main loop that works in a forked process
 	while self.flag == True:
+	    # get group data as json and publish to /groups topic
 	    msg = js.get_groups_json(hubip, securityid)
 	    for _ in range(len(msg)):
 		msg_json = json.loads(msg[_])
 		id = str(msg_json["ID"])
-		(result, mid) = client.publish(topic+"/groups/"+id, str(msg_json), 1, True)
+		(result, mid) = client.publish(topic+"/groups"+id, str(msg_json), 1, False)
 		if result == mqtt.MQTT_ERR_SUCCESS:
 		    pass
 		else:
     		    sys.stderr.write(result+'\n')
-    		    sys.stderr.flush()
-
+	    # get devices data as json and publis to /devices topic
 	    msg = js.get_ldevs_json(hubip, securityid)
 	    for _ in range(len(msg)):
 		msg_json = json.loads(msg[_])
 		id = str(msg_json["ID"])
-		(result, mid) = client.publish(topic+"/devices/"+id, str(msg_json), 1, True)
+		(result, mid) = client.publish(topic+"/devices"+id, str(msg_json), 1, False)
 		if result == mqtt.MQTT_ERR_SUCCESS:
 		    pass
 		else:
     		    sys.stderr.write(result+'\n')
-    		    sys.stderr.flush()
-
+	    # we are running, wait for a while
 	    running = True
     	    time.sleep(1)
-
+	# we're not running anymore, unsubscribe and disconnect
 	running = False
-	client.unsubscribe(topic)
-	time.sleep(1)
+	client.unsubscribe([topic+"/devices/todo", topic+"/groups/todo"])
+	time.sleep(0.5)
 	client.disconnect()
-	time.sleep(1)
+	time.sleep(0.5)
 
-def on_connect(client, obj, flags, rc):
+def on_connect(client, userdata, flags, rc):
     sys.stderr.write("Connected with result code "+str(rc)+"\n")
-    sys.stderr.flush()
     time.sleep(0.1)
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     #client.subscribe(topic)
 
-def on_disconnect(client, obj, rc):
+def on_disconnect(client, userdata, rc):
     if rc != mqtt.MQTT_ERR_SUCCESS:
         sys.stderr.write("Unexpected disconnection"+str(rc)+"\n")
     else:
 	sys.stderr.write("Disconnected succesfully\n")
 	client.loop_stop()
-	time.sleep(0.1)
-    sys.stderr.flush()
+	time.sleep(0.2)
 
-def on_message(client, obj, msg):
-    sys.stderr.write("message: "+msg.topic+" "+str(msg.payload)+"\n")
-    sys.stderr.flush()
+def on_message(client, userdata, msg):
+    sys.stderr.write("message on: "+msg.topic+" : "+str(msg.payload)+"\n")
+    # check message content
+    try:
+	m = json.loads(msg.payload)
+	id = m["ID"]
+	st = m["State"]
+    except ValueError:
+	pass
+    # check current status st of device id and change accordingly
 
-def on_publish(client, obj, mid):
+def on_publish(client, userdata, mid):
     sys.stderr.write("message sent: " + str(mid)+"\n")
-    sys.stderr.flush()
 
-def on_subscribe(client, obj, mid, granted_qos):
+def on_subscribe(client, userdata, mid, granted_qos):
     sys.stderr.write("Subscribed: " + str(mid) + " " + str(granted_qos)+"\n")
-    sys.stderr.flush()
+    
+def on_unsubscribe(client, userdata, mid):
+    sys.stderr.write("Unsubscribed: " + str(mid) + "\n")
 
-def on_log(client, obj, level, string):
+def on_log(client, userdata, level, string):
     sys.stderr.write(string+"\n")
-    sys.stderr.flush()
 
 def setup():
     global client
-
+    # read configuration from cfg file
     conf = ConfigParser.ConfigParser()
     conf.read(cfg)
     mqtt_srv = conf.get('tradfri', 'mqtt_srv')
@@ -103,18 +113,19 @@ def setup():
     topic = conf.get('tradfri', 'topic')
     hubip = conf.get('tradfri', 'hubip')
     securityid = conf.get('tradfri', 'securityid')
-
+    # define callback methods
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
     client.on_subscribe = on_subscribe
+    client.on_unsubscribe = on_unsubscribe
     client.will_set = (topic, "Tradfri went offline!", 1, True)
     #client.on_log = on_log
 
     return mqtt_srv, mqtt_port, topic, hubip, securityid
 
 def main():
-    global flag
+    # create new daemon
     daemon = MyDaemon('/tmp/tradfri-daemon.pid', '/dev/null', '/dev/null', '/dev/stderr')
     if len(sys.argv) == 2:
 	if 'start' == sys.argv[1]:
