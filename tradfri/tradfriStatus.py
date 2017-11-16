30374096d32e7e02243e7b581e8a67b39b1e0e70
Andrea Prunic <aprunic@gmail.com>
2017-11-16 15:37:01 +0100

    - fix wrong api call in send()

--- tradfri/tradfriStatus.py
+++ tradfri/tradfriStatus.py
@@ -17,8 +17,8 @@ global coap
 coap = '/usr/local/bin/coap-client'
 ###coap-client  -m get  -u "Confuzed22" -k "6V6TYU3VlkR13TWp" "coaps://10.20.30.25:5684/15001/65537" 2>/dev/stdout|awk 'NR==2'
 
-def send(ident, psk, payload, tradfriHub):
-    api = '{} -m put -u "{}" -k "{}" -e \'{}\' "{}"' .format(coap, ident, psk, payload, tradfriHub)
+def send(ident, psk, tradfriHub):
+    api = '{} -m get -u "{}" -k "{}" "{}"'.format(coap, ident, psk, tradfriHub)
 
     try:
 	p1 = subprocess.Popen(split(api), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=False)
