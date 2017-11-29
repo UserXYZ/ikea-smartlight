50add17a8a51c57bdc9861ec9996bb00db78cbce
Andrea Prunic <aprunic@gmail.com>
2017-11-29 17:59:04 +0100

    - add method to rewrite config file to add randomly generated ident and psk based on it. This method gets called every time main program is started, so that each new session has a new ident and psk.

--- tradfri/tradfriHelper.py
+++ tradfri/tradfriHelper.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-import sys, time
+import os, sys, time, ConfigParser
+from .tradfriActions import get_psk as get_psk
 
 def errmsg(err):
     if err:
 	t=time.strftime("%c",time.localtime())+" : "
 	sys.stderr.write(t+err+"\n")
+
+def make_cfg(cfg, hubip, securityid):
+    conf=ConfigParser.ConfigParser()
+    if conf.read(cfg):
+	import uuid
+	ident = uuid.uuid4()
+	conf.set('tradfri','ident', ident)
+	psk = get_psk(hubip, securityid, ident)
+	if psk == False:
+	    errmsg("Can't get PSK!")
+	    return False
+	else:
+	    conf.set('tradfri','psk', psk)
+	    try:
+	        file = open(cfg, 'w')
+		conf.write(file)
+		file.close()
+	    except IOError:
+    	        errmsg("Can't open config file for writing!"), arg
+    	        return False
+	return True
+    else:
+	errmsg("No config file!")
+	return False
